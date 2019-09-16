bd8b837d0d8b313a7628933abf74dc51e5602962
Bachir Khiati <bachir.khiati@gmail.com>
2019-09-16 11:25:40 +0300

    Fix project/cleaning

---
+++ ios/RNVideoProcessing-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  RNVideoTrimmer-Bridging-Header.h
+//  RNVideoProcessing
+//
+
+#import <React/RCTBridgeModule.h>
+#import <React/RCTBridge.h>
+#import <React/RCTEventDispatcher.h>
+#import <React/RCTRootView.h>
+#import <React/RCTUtils.h>
+#import <React/RCTConvert.h>
+#import <React/RCTBundleURLProvider.h>
+#import "SDAVAssetExportSession.h"
